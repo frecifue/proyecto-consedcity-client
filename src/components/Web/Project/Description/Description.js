028362f24fd814dbdc20dec0c2ba88ada25a9c5d
fredy cifuentes <fcifuentes@ubiobio.cl>
2025-09-06 19:20:36 -0400

    Proyectos
    - se agrega texto de content
    - se agrega margen a las secciones de about, ourwork y project description

--- src/components/Web/Project/Description/Description.js
+++ src/components/Web/Project/Description/Description.js
@@ -1,18 +1,15 @@
 import React from "react";
-// import { GeneralInfo } from "../../../../api";
 import { Loader } from "semantic-ui-react";
-// import "./About.scss";
-
+import "./Description.scss"
 
 export function Description({name, description}) {
     
-
     if (!description) return <Loader active inline="centered" />;
     
     return (
-        <section className="about-section" id="about-section">
+        <section className="project-section" id="project-section">
             <div className="container">
-                <h2 className="about-section__title">{name}</h2>
+                <h2 className="project-section__title">{name}</h2>
                 <p className="description">{description}</p>
             </div>
         </section>
